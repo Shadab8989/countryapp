36a82277c5514df17e56ada76560885aba90cb70
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2024-01-06 00:05:18 +0530

    trial1

---
+++ src/filteredResults.js
@@ -0,0 +1,5 @@
+const FilteredResults = () => {
+
+}
+
+export default FilteredResults
--- src/search.js
+++ src/search.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState,useContext } from 'react';
 import "./search.css"
+import CountriesDataArray from "./context/createContext.js";
+
 
 
 function SearchField({ handleSubmit }) {
+
+  const data = useContext(CountriesDataArray);
+  const countryname = data.map(country=>country.name.common)
+  // console.log(countryname)
+
+  const [input,setInput] = useState('')
+  function handleChange(value){
+    setInput(value)
+    const results = input && countryname.filter(country=> country.toLowerCase().includes(input));
+    console.log(results)
+  }
   return (
-    <form onSubmit={handleSubmit} className="search-field">
+    <>
+      <form onSubmit={handleSubmit} className="search-field">
       <label>
-        <input className="search-input" type="text" name="country" autoComplete='off' placeholder='Search Country here'/>
+        <input className="search-input" type="text" name="country" autoComplete='off' placeholder='Search Country here' onChange={(e) => {handleChange(e.target.value)}}/>
       </label>
       <button type="submit" className="search-button">Search</button>
     </form>
+    <div className="search-results">Search Results</div>
+    </>
   );
 }
 
